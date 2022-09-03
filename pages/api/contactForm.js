c51e95b9c9eed7ff88233b526f95be7eea21c0db
EbadHafeez <ebadhafeez0504@gmail.com>
2022-09-04 01:59:05 +0500

    Update contactForm.js

--- pages/api/contactForm.js
+++ pages/api/contactForm.js
@@ -6,29 +6,32 @@ const { SG_API_KEY, FROM_EMAIL, TO_EMAIL } = process.env
 sgMail.setApiKey(SG_API_KEY)
 
 export default async function formData(req, res) {
+    try {
+        db().then(() => {
+            console.log("DB Connected")
+        }).catch(err => console.error(err))
 
-    db().then(() => {
-        console.log("DB Connected")
-    }).catch(err => console.error(err))
+        const data = new contactForm_data(req.body)
+        data.save().then(() => {
+            console.log("Form Submitted")
+        }).catch(err => {
+            res.status(400).json("Error:" + err)
+        })
 
-    const data = new contactForm_data(req.body)
-    data.save().then(() => {
-        console.log("Form Submitted")
-    }).catch(err => {
-        res.status(400).json("Error:" + err)
-    })
+        const msg = {
+            to: TO_EMAIL,
+            from: FROM_EMAIL,
+            subject: "Query from Webite",
+            html: `<p> <strong>Name:</strong> ${data.name}</p>
+                <p><strong>Email:</strong> ${data.email}</p>
+                <p><strong>Phone:</strong> ${data.phone}</p>
+                <p><strong>Service:</strong> ${data.service}</p>
+                <p><strong>Message:</strong> ${data.message}</p>`
+        }
 
-    const msg = {
-        to: TO_EMAIL,
-        from: FROM_EMAIL,
-        subject: "Query from Webite",
-        html: `<p> <strong>Name:</strong> ${data.name}</p>
-        <p><strong>Email:</strong> ${data.email}</p>
-        <p><strong>Phone:</strong> ${data.phone}</p>
-        <p><strong>Service:</strong> ${data.service}</p>
-        <p><strong>Message:</strong> ${data.message}</p>`
+        await sgMail.send(msg)
+        res.status(200).json({ message: "Email Sent" })
+    } catch (e) {
+        console.log(e)
     }
-
-    await sgMail.send(msg)
-    res.status(200).json({ message: "Email Sent" })
 }
